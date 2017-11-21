2c98ac44446234f192c05f8e29abc1e283925dde
Tyler Smith <tyler@technologytherapy.com>
2017-11-21 10:14:12 -0500

    Template

--- src/templates.js
+++ src/templates.js
@@ -54,9 +54,7 @@ const loadTemplates = async (n, resources, Vue) => {
           const style = insertScope(`${template.css || ''}`, `.template-${template.id}`)
           const stringTemplate = `
           <template>
-            <div id="template-${template.id}" class="template-${template.id}" >       
               ${template.html || ''}
-            </div>
           </template>
           <style > ${style} </style>
           <script>
--- test.js
+++ test.js
@@ -7,7 +7,9 @@ client = new ApolloClient({
   link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
   cache: new InMemoryCache()
 });
-// const attachToResource = require('./src/attachToResource')(client)
-require('./src/templates')({prototype: {apolloClient: client } })
-.then(console.log)
-.catch(console.error)
+const attachToResource = require('./src/attachToResource')(client)
+// require('./src/templates')({prototype: {apolloClient: client } })
+// .then(console.log)
+// .catch(console.error)
+
+attachToResource(2)
