03ba029a69d58224cefb8f8a85afdc7129129e19
Tyler Smith <tyler@technologytherapy.com>
2017-12-17 15:00:07 -0500

    Seeder and field updates

--- index.js
+++
@@ -1,8 +0,0 @@
-const attachToResource = require('./src/attachToResource')
-const templates = require('./src/templates')
-
-module.exports = {
-  seeder: require('./src/seeder'),
-  attachToResource,
-  templates
-}
